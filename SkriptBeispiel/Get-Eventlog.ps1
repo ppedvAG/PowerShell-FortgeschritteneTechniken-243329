61aacbb9cb9434ac9dacf76096b4dca72f7e88a4
Stefan Ober <StefanO@ppedv.de>
2024-11-14 13:29:42 +0100

    Skript mit Parametern Simple

---
+++ SkriptBeispiel/Get-Eventlog.ps1
@@ -0,0 +1,8 @@
+﻿param(
+$EventId,
+$Newest,
+$computerName = "localhost"
+)
+
+Get-EventLog -LogName Security -ComputerName $computerName | Where-Object -FilterScript {$PSItem.EventID -eq $EventId} | Select-Object -First $Newest
+
