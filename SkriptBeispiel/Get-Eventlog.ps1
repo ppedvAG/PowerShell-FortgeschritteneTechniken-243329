32a4018741f4723a87d3ee142a3214149c39f6be
Stefan Ober <StefanO@ppedv.de>
2024-11-14 13:38:44 +0100

    Skript mit Verbose Ausgabe

--- SkriptBeispiel/Get-Eventlog.ps1
+++ SkriptBeispiel/Get-Eventlog.ps1
@@ -1,4 +1,5 @@
-﻿param(
+﻿[cmdletBinding()]
+param(
 [Parameter(Mandatory=$true)]
 [int]$EventId,
 
@@ -7,5 +8,9 @@
 [string]$ComputerName = "localhost"
 )
 
+Write-Verbose -Message "ZUsätzliche Ausgaben"
+Write-Verbose -Message "Vom User übergeben: $EventId , $Newest , $ComputerName"
+
+Write-Verbose -Message "Vor der Abfrage"
 Get-EventLog -LogName Security -ComputerName $computerName | Where-Object -FilterScript {$PSItem.EventID -eq $EventId} | Select-Object -First $Newest
 
